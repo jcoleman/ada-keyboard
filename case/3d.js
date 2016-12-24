31f77ca5b2cb78e7f8f6632b0fb66b36b9ccf94a
James Coleman <jtc331@gmail.com>
2016-12-24 12:07:07 -0500

    Begin 3d case design work.

---
+++ case/3d.js
@@ -0,0 +1,30 @@
+// Measurements from ErgoDox design.
+var SWITCH_PLATE_THICKNESS = 3;
+var SWITCH_HOLE_SIZE = 13.97;
+var SWITCH_NOTCH_WIDTH = 3.5001;
+var SWITCH_NOTCH_DEPTH = 0.8128;
+
+// Returns the filled solid of a switch hole.
+function switchHole(opts={thickness: SWITCH_PLATE_THICKNESS}) {
+  var thickness = opts.thickness;
+  var holeRadius = SWITCH_HOLE_SIZE / 2;
+  var plateRadius = thickness / 2;
+  var square = CSG.cube({
+    radius: [holeRadius, holeRadius, plateRadius],
+  });
+  var notch = CSG.cube({
+    radius: [SWITCH_NOTCH_DEPTH / 2, SWITCH_NOTCH_WIDTH / 2, plateRadius],
+  });
+  var notchCenterOffset = holeRadius - SWITCH_NOTCH_DEPTH / 2;
+
+  return square.subtract(
+    notch.translate([notchCenterOffset, 0])
+  ).subtract(
+    notch.translate([-notchCenterOffset, 0])
+  );
+}
+
+function main() {
+  return switchHole();
+}
+
