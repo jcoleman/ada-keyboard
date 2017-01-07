ec0badd1896ede3ab671d79e01d207f1a3239ddb
James Coleman <jtc331@gmail.com>
2017-01-07 18:06:38 -0500

    First pass at adding HDMI/USB connector cutouts.

--- case/3d.js
+++ case/3d.js
@@ -7,6 +7,17 @@ var SWITCH_NOTCH_DEPTH = 0.8128;
 var SWITCH_CENTER_Y_SPACING = 19;
 var SWITCH_CENTER_X_SPACING = 19;
 
+function installLibraryExtensions() {
+  CSG.prototype.getBoundsCenter = function() {
+    var bounds = this.getBounds();
+    return new CSG.Vector3D([
+      (bounds[0].x + bounds[1].x) / 2,
+      (bounds[0].y + bounds[1].y) / 2,
+      (bounds[0].z + bounds[1].z) / 2,
+    ]);
+  }
+}
+
 // Returns the filled solid of a switch hole.
 function switchHole(opts={thickness: SWITCH_PLATE_THICKNESS}) {
   var thickness = opts.thickness;
@@ -211,13 +222,37 @@ function switchPlateLeftHand() {
   // Layout initial relative switch positions for thumb matrix (required for hull calculation).
   connectSwitchesInDescriptorMatrix(thumbMatrix, {center: true});
 
-  var primaryExteriorHull = hullForMatrix(primaryMatrix, {radius: 10, offset: {bottom: -25}});
-  var primaryInteriorHull = hullForMatrix(primaryMatrix, {radius: 3});
+  var exteriorHullSwitchRadius = 10;
+  var interiorHullSwitchRadius = 3;
+  var primaryExteriorHullOffsets = {bottom: -25, top: 5};
+
+  var primaryExteriorHull = hullForMatrix(primaryMatrix, {radius: exteriorHullSwitchRadius, offset: primaryExteriorHullOffsets});
+  var primaryExteriorBounds = primaryExteriorHull.getBounds();
+  for (var i = 0; i < primaryExteriorHull.sides.length; ++i) {
+    var side = primaryExteriorHull.sides[i];
+    if (side.vertex0.pos.x == primaryExteriorBounds[1].x && side.vertex1.pos.x == primaryExteriorBounds[1].x) {
+      side.vertex1.pos = new CSG.Vector2D(side.vertex1.pos.x, primaryExteriorBounds[1].y);
+      var side2 = primaryExteriorHull.sides[i + 1];
+      side2.vertex0.pos = new CSG.Vector2D(side2.vertex0.pos.x, primaryExteriorBounds[1].y);
+      break;
+    }
+  }
+  var primaryInteriorHull = hullForMatrix(primaryMatrix, {radius: interiorHullSwitchRadius});
+  var primaryInteriorBounds = primaryInteriorHull.getBounds();
+  for (var i = 0; i < primaryInteriorHull.sides.length; ++i) {
+    var side = primaryInteriorHull.sides[i];
+    if (side.vertex0.pos.x == primaryInteriorBounds[1].x && side.vertex1.pos.x == primaryInteriorBounds[1].x) {
+      side.vertex1.pos = new CSG.Vector2D(side.vertex1.pos.x, primaryInteriorBounds[1].y);
+      var side2 = primaryInteriorHull.sides[i + 1];
+      side2.vertex0.pos = new CSG.Vector2D(side2.vertex0.pos.x, primaryInteriorBounds[1].y);
+      break;
+    }
+  }
   var primaryPlate = linear_extrude({height: SWITCH_PLATE_THICKNESS}, primaryExteriorHull);
   var primaryInteriorCutout = linear_extrude({height: SWITCH_PLATE_THICKNESS}, primaryInteriorHull);
 
-  var thumbExteriorHull = hullForMatrix(thumbMatrix, {radius: 10});
-  var thumbInteriorHull = hullForMatrix(thumbMatrix, {radius: 3});
+  var thumbExteriorHull = hullForMatrix(thumbMatrix, {radius: exteriorHullSwitchRadius});
+  var thumbInteriorHull = hullForMatrix(thumbMatrix, {radius: interiorHullSwitchRadius});
   var thumbPlate = linear_extrude({height: SWITCH_PLATE_THICKNESS}, thumbExteriorHull);
   var thumbInteriorCutout = linear_extrude({height: SWITCH_PLATE_THICKNESS}, thumbInteriorHull);
 
@@ -283,13 +318,80 @@ function switchPlateLeftHand() {
 
   var fullPlate = primaryMatrixDescriptor.plate.union(thumbMatrixDescriptor.plate);
   var fullInteriorCutout = primaryMatrixDescriptor.cutout.union(thumbMatrixDescriptor.cutout);
-  var fullSpacer = fullPlate.subtract(fullInteriorCutout);
+  var fullSpacer = fullPlate.subtract(fullInteriorCutout).scale([1, 1, 2]);
+  fullSpacer = fullSpacer.translate([0, 0, -fullSpacer.getBounds()[0].z]);
+
+  // Add cutouts for HDMI connector.
+  var spacerHeight = fullSpacer.getBounds()[1].z - fullSpacer.getBounds()[0].z;
+  var hullSwitchRadiusDifferential = exteriorHullSwitchRadius - interiorHullSwitchRadius;
+  var hdmiCutout = CSG.cube({radius: [15.4/2, (hullSwitchRadiusDifferential + primaryExteriorHullOffsets.top) / 2, spacerHeight / 2]});
+  hdmiCutout = hdmiCutout.translate([0, 0, -hdmiCutout.getBounds()[0].z]);
+  hdmiCutout.properties.topRightConnector = new CSG.Connector(
+    [hdmiCutout.getBounds()[1].x, hdmiCutout.getBounds()[1].y, 0],
+    [0, 0, 1],
+    [0, 1, 0]
+  );
+  fullSpacer.properties.hdmiCutoutConnector = new CSG.Connector(
+    [primaryMatrixDescriptor.plate.getBounds()[1].x - hullSwitchRadiusDifferential, primaryMatrixDescriptor.plate.getBounds()[1].y, 0],
+    [0, 0, 1],
+    [0, 1, 0]
+  );
+  hdmiCutout = hdmiCutout.connectTo(
+    hdmiCutout.properties.topRightConnector,
+    fullSpacer.properties.hdmiCutoutConnector,
+    false,
+    0
+  );
+
+  var usbCutout = CSG.cube({radius: [8.1/2, (hullSwitchRadiusDifferential + primaryExteriorHullOffsets.top + 1.5) / 2, spacerHeight / 2]});
+  usbCutout = usbCutout.translate([0, 0, -usbCutout.getBounds()[0].z]);
+  usbCutout.properties.topCenterConnector = new CSG.Connector(
+    [0, usbCutout.getBounds()[1].y, 0],
+    [0, 0, 1],
+    [0, 1, 0]
+  );
+  var usbCutoutPlateTopSide = null;
+  for (var i = 0; i < primaryExteriorHull.sides.length; ++i) {
+    var side = primaryExteriorHull.sides[i];
+    if (side.vertex0.pos.x == primaryExteriorBounds[0].x && side.vertex1.pos.x == primaryExteriorBounds[0].x) {
+      usbCutoutPlateTopSide = primaryExteriorHull.sides[i - 1];
+      break;
+    }
+  }
+  var usbCutoutPlateTopRiserVertex0 = new CAG.Vertex(new CSG.Vector2D([usbCutoutPlateTopSide.vertex1.pos.x, usbCutoutPlateTopSide.vertex0.pos.y]));
+  var usbCutoutPlateTopRiser = new CAG.Side(
+    usbCutoutPlateTopRiserVertex0,
+    usbCutoutPlateTopSide.vertex1
+  );
+  var usbCutoutAngle = Math.asin(
+    usbCutoutPlateTopRiser.length() / usbCutoutPlateTopSide.length()
+  ) * (180 / Math.PI);
+  var usbCutoutCenterPlateX = primaryMatrix[0][1].keySwitch.getBoundsCenter().x;
+  var usbCutoutCenterPlateY = primaryMatrixDescriptor.plate.getBounds()[1].y - (
+    usbCutoutPlateTopRiser.length() * (
+      (usbCutoutPlateTopSide.vertex0.pos.x - usbCutoutCenterPlateX) / (usbCutoutPlateTopSide.vertex0.pos.x - usbCutoutPlateTopSide.vertex1.pos.x)
+    )
+  );
+  fullSpacer.properties.usbCutoutConnector = new CSG.Connector(
+    [usbCutoutCenterPlateX, usbCutoutCenterPlateY, 0],
+    [0, 0, 1],
+    [0, 1, 0]
+  );
+  usbCutout = usbCutout.connectTo(
+    usbCutout.properties.topCenterConnector,
+    fullSpacer.properties.usbCutoutConnector,
+    false,
+    usbCutoutAngle
+  );
+
+  fullSpacer = fullSpacer.subtract(hdmiCutout).subtract(usbCutout);
   var switchPlate = fullPlate.subtract(primaryMatrixDescriptor.switches.union(thumbMatrixDescriptor.switches));
 
-  return switchPlate.translate([0, 0, SWITCH_PLATE_THICKNESS]).union(fullSpacer);
+  return switchPlate.translate([0, 0, fullSpacer.getBounds()[1].z]).union(fullSpacer);
 }
 
 function main() {
+  installLibraryExtensions();
   return switchPlateLeftHand();
 }
 
