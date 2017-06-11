ae6e365843cee073dbe13524b2ab5c852d77dbd4
James Coleman <jtc331@gmail.com>
2017-06-11 16:16:29 -0400

    Add further sanity testing for full build process.

--- __tests__/case/sanity_spec.js
+++ __tests__/case/sanity_spec.js
@@ -14,4 +14,21 @@ describe('Keyboard', () => {
       });
     });
   });
+
+  it('builds the full part with the expected bounds', () => {
+    const promise = generateOutputData({renderedPart: 'full'}, {inputFile: "case/main.jscad"});
+    return promise.then(objects => {
+      expect(objects).toBeInstanceOf(Array);
+      expect(objects.length).toBe(1);
+      expect(objects[0].getBounds()).toEqual(
+        // Magic values from previously visually confirmed valid run
+        // to be able to sanity check that refactors don't completely
+        // break rendering.
+        [
+          {"_x": -75.80851303331261, "_y": -81.04444872241592, "_z": 0},
+          {"_x": 75.80851303331261, "_y": 81.04444872241592, "_z": 57.36726290341663}
+        ]
+      );
+    });
+  });
 });
