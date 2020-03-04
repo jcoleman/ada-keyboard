03316443abd5d505a3d3ed8239fc062376a38269
James Coleman <jtc331@gmail.com>
2020-03-03 21:37:00 -0500

    Improve split keyboard switch plate sanity spec.
    Asserting that building the switch plate doesn't blow up provides fairly
    low resolution test feedback; instead verify the bounds of the generated
    object (granted, with magic numbers) so that refactoring can at least
    mostly verify that nothing's obviously changed without visually
    confirmed in the browser-based renderer.
    
    We already have this style of test for rendering the full part, but it's
    useful while doing significant refactoring to be able to see if a
    problem lies at the switch plate level or only in the full part with
    base.

--- __tests__/case/sanity_spec.js
+++ __tests__/case/sanity_spec.js
@@ -5,13 +5,24 @@ global.CSG = jscad.CSG;
 global.CAG = jscad.CAG;
 
 describe('SplitKeyboard', () => {
-  it('builds the switch plate without blowing up', () => {
+  it('builds the switch plate with the expected bounds', () => {
     const promise = generateOutputData({keyboardStyle: "splitLeft", renderedPart: 'switchPlate'}, {inputFile: "case/main.js"});
     return promise.then(objects => {
       expect(objects).toBeInstanceOf(Array);
+      expect(objects.length).toBe(1);
       objects.forEach(object => {
         expect(object).toBeInstanceOf(CSG);
       });
+
+      expect(objects[0].getBounds()).toEqual(
+        // Magic values from previously visually confirmed valid run
+        // to be able to sanity check that refactors don't completely
+        // break rendering.
+        [
+          {"_x": -78.73502864150389, "_y": -80.88082342452822, "_z": 0},
+          {"_x": 78.73502864150389, "_y": 80.88082342452822, "_z": 6.0000000000000036}
+        ]
+      );
     });
   });
 
